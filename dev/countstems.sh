3c0ae3985eb4a50a4e12dad691c4ebcbe116bfcb
Jonathan North Washington <jonathan.n.washington@gmail.com>
2012-08-20 06:59:37 +0000

    script to count number of stems in apertium-kaz

---
+++ dev/countstems.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+LEXC=../apertium-kaz.kaz.lexc
+
+allcounts=`hfst-lexc --format foma $LEXC -o /dev/null | grep Root | sed 's/,/\n/g'`
+
+for line in $allcounts; do
+	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
+	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
+	if [[ "$thing" == "Root" ]]; then
+		rootsize=$numbr;
+		root=`grep -A$rootsize Root $LEXC | grep -v Root`;
+	fi;
+done;
+
+root=`echo $root | sed -r 's/\s*;/\n/g'`;
+
+string=""
+for line in $allcounts; do
+	thing=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\1/'`;
+	numbr=`echo $line | sed -r 's/(.*)\.\.\.([0-9]*),?/\2/'`;
+	for thingname in $root; do
+		if [ "$thing" = "$thingname" ]; then
+			string="$string + $numbr";
+		fi;
+	done;
+done;
+
+echo `calc $string`;
