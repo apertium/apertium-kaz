b81216de8e33f886dc94788875956a36b3ab4ecd
Jonathan North Washington <jonathan.n.washington@gmail.com>
2014-05-31 15:13:42 +0000

    ignorable rule conflicts, but very accurate transliteration

--- dev/transliterate/test.sh
+++ dev/transliterate/test.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
 
-echo "تاقتايشا" | hfst-proc ara-kaz.ohfst
+echo "تاقتايشا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be тақтайша
 
-echo "ايىرماسى" | hfst-proc ara-kaz.ohfst
+echo "ايىرماسى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be айырмасы
 
-echo "زەكي" | hfst-proc ara-kaz.ohfst
+echo "زەكي" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be зеки
 
-echo "قيناپ" | hfst-proc ara-kaz.ohfst
+echo "قيناپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be қинап
 
-echo "كوپ" | hfst-proc ara-kaz.ohfst
+echo "كوپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be көп
 
-echo "سولتۇستىك" | hfst-proc ara-kaz.ohfst
+echo "سولتۇستىك" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be солтүстік
 
-echo "تەرموس" | hfst-proc ara-kaz.ohfst
+echo "تەرموس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be термос
 
-echo "ۇيا" | hfst-proc ara-kaz.ohfst
+echo "ۇيا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
 # should be ұя
 
+echo "مويناق" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+
+echo "كيىم" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+#should be киім
+echo "كيۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+#should be кию
+echo "كيىنىس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+#should be киініс
+
+echo "قويۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+# should be қою
