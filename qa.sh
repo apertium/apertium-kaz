c04a63c2412ca60048d6b1a58b86f4052d0e874c
Ilnar Salimzyan <ilnar@selimcan.org>
2014-07-04 12:14:19 +0000

    look for test scripts in 'test-scripts'

--- qa.sh
+++ qa.sh
@@ -1,6 +1,15 @@
-# Takes the basename of the test scrpt in /tests as an argument
-# and runs the test.
-
 #!/bin/bash
 
-bash "tests/$1.test"
+# Takes the basename of the test scrpt in /test-scripts as an argument,
+# an addtional argument if the test requires it, and runs the test.
+#
+# Usage: ./qa.sh (will default to './qa.sh kaz')
+
+if [ $# -eq 0 ]
+then
+    testToRun=kaz.test
+else
+    testToRun=$1.test
+fi
+
+bash "test-scripts/$testToRun" "$2"
