ab3d0f4a3086cf30133bdf222e19f78b2d538d9d
Zhenisbek Assylbekov <zhassylbekov@nu.edu.kz>
2015-01-27 06:06:32 +0000

    fix url in links.pl

--- texts/akorda/links.pl
+++ texts/akorda/links.pl
@@ -7,16 +7,16 @@ use Data::Dumper;
 my $LX = new HTML::LinkExtractor();
 my $counter = 0;
 
-for (my $page=1; $page<=370; $page++)
+for (my $page=1; $page<=391; $page++)
 {
-	$content = get("http://akorda.kz/pda.php?r=page/allNews&category_id=127&language=en&Pages_page=$page");
+	$content = get("http://akorda.kz/pda.php/en/allNews?category_id=88&Pages_page=$page");
 	die "Couldn't get it!" unless defined $content;
 
 	$LX->parse(\$content);
 
 	for my $Link ( @{ $LX->links } )
 	{
-		if ($$Link{href} =~ /\/pda.php\?r=page\/view&sefname=page_.+&language=en/)
+		if ($$Link{href} =~ /\/pda.php\/en\/page\/page_.+/)
 		{
 			print "http://akorda.kz".$$Link{href}."\n";
 			$counter++;
