ced1b89683753b309867e7a24661ec988178f93c
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-03-07 21:03:16 +0000

    working dayekshe behaviour with preprocessing

--- dev/transliterate/cyr-ara.sh
+++ dev/transliterate/cyr-ara.sh
@@ -2,5 +2,6 @@
 
 while read line
 do
-	echo "ء$line" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings 
+	dayeksheline=`echo "ء$line" | sed 's/ / ء/g'`
+	echo "$dayeksheline" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings 
 done < "${1:-/dev/stdin}"
