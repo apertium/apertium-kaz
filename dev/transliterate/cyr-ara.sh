1d5981d5875233baec467caf1c87372a26eec409
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-03-07 06:29:24 +0000

    better working

--- dev/transliterate/cyr-ara.sh
+++ dev/transliterate/cyr-ara.sh
@@ -2,5 +2,5 @@
 
 while read line
 do
-	echo "$line" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings 
+	echo "ء$line" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings 
 done < "${1:-/dev/stdin}"
