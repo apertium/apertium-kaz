dc9f2fd5fb2ee5e28a0163c893361c64a446fbdc
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-03-11 05:33:22 +0000

    really good performance

--- dev/transliterate/cyr-ara.sh
+++ dev/transliterate/cyr-ara.sh
@@ -2,6 +2,7 @@
 
 while read line
 do
-	dayeksheline=`echo "ء$line" | sed 's/ / ء/g'`
-	echo "$dayeksheline" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings 
+	dayeksheline=`echo "ء$line" | sed 's/ / ء/g' | sed 's/-/-ء/g'`
+	outline=`echo "$dayeksheline" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings`
+	echo $outline | sed -r 's/(.*):(.*)/\2/'
 done < "${1:-/dev/stdin}"
