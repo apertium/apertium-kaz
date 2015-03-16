8a16d8bf4b1c78b693281eca40ea5ccfba237353
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-03-16 05:04:28 +0000

    a few minor improvements

--- dev/transliterate/cyr-ara.sh
+++ dev/transliterate/cyr-ara.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 while read line
 do
 	dayeksheline=`echo "ء$line" | sed 's/ / ء/g' | sed 's/-/-ء/g'`
-	outline=`echo "$dayeksheline" | hfst-strings2fst  | hfst-compose-intersect -2 cyr-ara.hfst  | hfst-fst2strings`
+	outline=`echo "$dayeksheline" | hfst-strings2fst  | hfst-compose-intersect -2 $SCRIPTPATH/cyr-ara.hfst  | hfst-fst2strings`
 	echo $outline | sed -r 's/(.*):(.*)/\2/'
 done < "${1:-/dev/stdin}"
