6020808f868bf1618f408ed1db6c1448e1b0cd8f
Jonathan North Washington <jonathan.n.washington@gmail.com>
2017-10-06 08:09:24 +0000

    updates

--- dev/transliterate/test.sh
+++ dev/transliterate/test.sh
@@ -1,52 +1,52 @@
 #!/bin/bash
 
-echo "تاقتايشا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "تاقتايشا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be тақтайша
 
-echo "ايىرماسى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "ايىرماسى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be айырмасы
 
-echo "زەكي" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "زەكي" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be зеки
 
-echo "قيناپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "قيناپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be қинап
 
-echo "كوپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "كوپ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be көп
 
-echo "سولتۇستىك" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "سولتۇستىك" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be солтүстік
 
-echo "تەرموس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "تەرموس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be термос
 
-echo "ۇيا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "ۇيا" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be ұя
 
-echo "مويناق" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "مويناق" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 
-echo "كيىم" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "كيىم" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 #should be киім
-echo "كيۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "كيۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 #should be кию
-echo "كيىنىس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "كيىنىس" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 #should be киініс
 
-echo "قويۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "قويۋ" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be қою
 
-echo "توركىن" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "توركىن" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # hrrm
 
-echo "سۇرتكى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "سۇرتكى" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be сүрткі
 
-echo "رايون" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "رايون" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be район
 
-echo "بەكەر" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "بەكەر" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be бекер
 
-echo "پەرىشتە" | hfst-strings2fst  | hfst-compose-intersect -2 ara-kaz.hfst  | hfst-fst2strings 
+echo "پەرىشتە" | hfst-strings2fst  | hfst-compose-intersect -2 ara-cyrl.hfst  | hfst-fst2strings 
 # should be періште
