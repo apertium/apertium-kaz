fee27e868272d6aefb5f582a5c32a7d3551a706c
Ilnar Salimzianov <ilnar@selimcan.org>
2019-02-21 00:22:03 +0300

    remove қой:қой V-IV ; !  from kaz.lexc

--- tests/vocabulary/expected.rkt
+++ tests/vocabulary/expected.rkt
@@ -47,19 +47,6 @@
                                              0 (- (string-length lexical) 1))))
                           (check-equal? (lu-contains? lu lexical) lexical))))]))
 
-;(define (test tc)
-;        (define surface (car tc))
-;        (define lexicals (car (cdr tc)))
-;        (define lu
-;                #{echo (values surface) | apertium -f none -d (values A-KAZ) kaz-morph})
-;        (for ([lexical lexicals])
-;             (if (string-suffix? lexical "!")
-;                 (check-false
-;                  (lu-contains? lu (substring
-;                                    lexical
-;                                    0 (- (string-length lexical) 1))))
-;                 (check-equal? (lu-contains? lu lexical) lexical))))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Main part: tests themselves
@@ -70,17 +57,47 @@
    ("ма<qst>")))
 
 (test
+ '(("ма не" "ме не")  ;; "ба не", "па не" etc? 
+                      ("ма не<qst>")))
+
+(test
+ '("ше"
+   ("ше<qst>")))
+
+(test
+ '("ғой"
+   ("ғой<mod_ass>")))
+
+(test
  '("қой"
    ("ғой<mod_ass>"
     "қой<n><nom>"
     "қой<n><attr>"
     "қой<vaux><imp><p2><sg>"
     "қой<v><tv><imp><p2><sg>"
-    "қой<v><iv><imp><p2><sg>!"  ;; ! at the end means that this analysis
+    "қой<v><iv><imp><p2><sg>!"
+    ;; ! mark at the end means that this analysis
     ;; should NOT be in the output of kaz-morph
     ;; for "қой" (as it is as of 20.02.2019)
     "қой<n><nom>+е<cop><aor><p3><sg>"
     "қой<n><nom>+е<cop><aor><p3><pl>")))
+
+(test
+ '("гөр"
+   ("гөр<mod_ass>")))
+
+(test
+ '("шығар"
+   ("шық<vaux><gpr_fut>"
+    "шық<v><iv><gpr_fut>"
+    "шығар<mod>"
+    "шық<vaux><gpr_fut><subst><nom>"
+    "шық<vaux><fut><p3><pl>"
+    "шық<vaux><fut><p3><sg>"
+    "шық<v><iv><gpr_fut><subst><nom>"
+    "шық<v><iv><fut><p3><pl>"
+    "шық<v><iv><fut><p3><sg>"
+    "шығар<v><tv><imp><p2><sg>")))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
