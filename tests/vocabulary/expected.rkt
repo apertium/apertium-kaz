ec750431ac1be199dd9c2a2893433384aad92ae9
Ilnar Salimzianov <ilnar@selimcan.org>
2019-02-26 18:42:36 +0300

    minor

--- tests/vocabulary/expected.rkt
+++ tests/vocabulary/expected.rkt
@@ -17,6 +17,7 @@
 
 
 (define A-KAZ '../../)
+(define A-KAZ-TAT '../../../../apertium-trunk/apertium-kaz-tat)
 (define WORDLIST (mutable-set))
 
 
@@ -26,31 +27,24 @@
 
 (define (test tc)
         (match tc
-               [(list (list surfaces ...)
-                      (list lexicals ...))
-                (for ([surface surfaces])
-                     (define lu
-                             #{echo (values surface) | apertium -f none -d (values A-KAZ) kaz-morph})
-                     (for ([lexical lexicals])
-                          (if (string-suffix? lexical "!")
-                              (check-false
-                               (lu-contains? lu (substring
-                                                 lexical
-                                                 0 (- (string-length lexical) 1))))
-                              (check-equal? (lu-contains? lu lexical) lexical))))]
-               [(list surface (list lexicals ...))
+               [(list surface (list expecteds ...))
                 (begin
                  (if (set-member? WORDLIST surface)
                      (error "There is already a test case for this word!")
                      (set-add! WORDLIST surface))
                  (define lu #{echo (values surface) | apertium -f none -d (values A-KAZ) kaz-morph})
-                 (for ([lexical lexicals])
-                      (if (string-suffix? lexical "!")
-                          (check-false
-                           (lu-contains? lu (substring
-                                             lexical
-                                             0 (- (string-length lexical) 1))))
-                          (check-equal? (lu-contains? lu lexical) lexical))))]))
+                 (for ([expected expecteds])
+                      (match expected
+                             [(list lexical slsent tlsent)
+                              (begin
+                               (if (string-suffix? lexical "!")
+                                   (check-false
+                                    (lu-contains? lu (substring
+                                                      lexical
+                                                      0 (- (string-length lexical) 1))))
+                                   (check-equal? (lu-contains? lu lexical) lexical))
+                               (define trans #{echo (values slsent) | apertium -f none -d (values A-KAZ-TAT) kaz-tat})
+                               (check-equal? tlsent trans))])))]))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -58,63 +52,85 @@
 
 
 (test
- '(("ма" "ба" "па" "ме" "бе" "пе")
-   ("ма<qst>")))
+ '("ма"
+   (("ма<qst>" "" ""))))
 
 (test
- '(("ма не" "ме не")  ;; "ба не", "па не" etc? 
-                      ("ма не<qst>")))
+ '("ба"
+   (("ма<qst>" "" ""))))
+
+(test
+ '("па"
+   (("ма<qst>" "" ""))))
+
+(test
+ '("ме"
+   (("ма<qst>" "" ""))))
+
+(test
+ '("бе"
+   (("ма<qst>" "" ""))))
+
+(test
+ '("пе"
+   (("ма<qst>" "" ""))))
+
+(test
+ '("ма не"   ;; "ба не", "па не" etc?
+       (("ма не<qst>" "" ""))))
+
+(test
+ '("ме не"
+       (("ма не<qst>" "" ""))))
 
 (test
  '("ше"
-   ("ше<qst>")))
+   (("ше<qst>" "Менің күйеуім ше?" "Минем киявем ничек?"))))
 
 (test
  '("ғой"
-   ("ғой<mod_ass>")))
+   (("ғой<mod_ass>" "" ""))))
 
 (test
  '("қой"
-   ("ғой<mod_ass>"
-    "қой<n><nom>"
-    "қой<n><attr>"
-    "қой<vaux><imp><p2><sg>"
-    "қой<v><tv><imp><p2><sg>"
-    "қой<v><iv><imp><p2><sg>!"
-    ;; ! mark at the end means that this analysis
-    ;; should NOT be in the output of kaz-morph
-    ;; for "қой" (as it is as of 20.02.2019)
-    "қой<n><nom>+е<cop><aor><p3><sg>"
-    "қой<n><nom>+е<cop><aor><p3><pl>")))
+   (("ғой<mod_ass>" "" "")
+    ("қой<n><nom>" "" "")
+    ("қой<n><attr>" "" "")
+    ("қой<vaux><imp><p2><sg>" "" "")
+    ("қой<v><tv><imp><p2><sg>" "" "")
+    ("қой<v><iv><imp><p2><sg>!" "" "")
+    ("қой<n><nom>+е<cop><aor><p3><sg>" "" "")
+    ("қой<n><nom>+е<cop><aor><p3><pl>" "" ""))))
 
 (test
  '("гөр"
-   ("гөр<mod_ass>")))
+   (("гөр<mod_ass>" "" ""))))
 
 (test
  '("шығар"
-   ("шық<vaux><gpr_fut>"
-    "шық<v><iv><gpr_fut>"
-    "шығар<mod>"
-    "шық<vaux><gpr_fut><subst><nom>"
-    "шық<vaux><fut><p3><pl>"
-    "шық<vaux><fut><p3><sg>"
-    "шық<v><iv><gpr_fut><subst><nom>"
-    "шық<v><iv><fut><p3><pl>"
-    "шық<v><iv><fut><p3><sg>"
-    "шығар<v><tv><imp><p2><sg>")))
+   (("шық<vaux><gpr_fut>" "" "")
+    ("шық<v><iv><gpr_fut>" "" "")
+    ("шығар<mod>" "" "")
+    ("шық<vaux><gpr_fut><subst><nom>" "" "")
+    ("шық<vaux><fut><p3><pl>" "" "")
+    ("шық<vaux><fut><p3><sg>" "" "")
+    ("шық<v><iv><gpr_fut><subst><nom>" "" "")
+    ("шық<v><iv><fut><p3><pl>" "" "")
+    ("шық<v><iv><fut><p3><sg>" "" "")
+    ("шығар<v><tv><imp><p2><sg>" "" ""))))
 
 (test
  '("да"
-   ("да<cnjcoo>"
-    "да<postadv>"
-    "да<adv>!")))
+   (("да<cnjcoo>" "" "")
+    ("да<postadv>" "" "")
+    ("да<adv>!" "" ""))))
 
 (test
  '("де"
-   ("да<cnjcoo>"
-    "да<postadv>"
-    "де<v><tv><imp><p2><sg>")))
+   (("да<cnjcoo>" "" "")
+    ("да<postadv>" "" "")
+    ("де<v><tv><imp><p2><sg>" "" ""))))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; alternative/additional test case formats
