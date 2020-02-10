bb8e40f653273f863cf40ad874048544d7df08ac
Ilnar Salimzianov <ilnar@selimcan.org>
2020-02-10 12:21:20 +0600

    add kaz-tagger mode to the Racket interface

--- main.rkt
+++ main.rkt
@@ -6,7 +6,7 @@
 ; https://taruen.github.io/apertiumpp/apertiumpp/ gives info on how to install
 ; it.
 
-(provide kaz-morph kaz-disam)
+(provide kaz-morph kaz-disam kaz-tagger)
 
 (require pkg/lib
          rackunit
@@ -27,3 +27,8 @@
   (parameterize ([current-directory (pkg-directory "apertium-kaz")])
     (rash
      "echo (values s) | apertium -n -d . kaz-morph | cg-proc kaz.rlx.bin")))
+
+(define (kaz-tagger s)
+  (parameterize ([current-directory (pkg-directory "apertium-kaz")])
+    (rash
+     "echo (values s) | apertium -n -d . kaz-tagger")))
