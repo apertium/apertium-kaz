d2269e85fd834c98aa7cb0c6f518a008c881f723
Ilnar Salimzianov <ilnar@selimcan.org>
2020-02-17 12:41:28 +0600

    few more mapping and attachement rules (sents 1-3 of ud treebank covered)

--- main.rkt
+++ main.rkt
@@ -6,17 +6,12 @@
 ; https://taruen.github.io/apertiumpp/apertiumpp/ gives info on how to install
 ; it.
 
-(provide kaz-morph kaz-tagger kaz-tagger-deterministic)
+(provide kaz-morph kaz-tagger kaz-tagger-deterministic kaz-disam)
 
 (require pkg/lib
          rackunit
          rash
          apertiumpp/streamparser)
-
-(define (symbol-append s1 s2)
-  (string->symbol (string-append (symbol->string s1) (symbol->string s2))))
-
-(define A-KAZ './)
 
 (define (kaz-morph s)
   (parameterize ([current-directory (pkg-directory "apertium-kaz")])
@@ -31,4 +26,9 @@
 (define (kaz-tagger-deterministic s)
   (parameterize ([current-directory (pkg-directory "apertium-kaz")])
     (rash
-     "echo (values s) | apertium -n -d . kaz-morph | cg-proc kaz.rlx.bin")))
+     "echo (values s) | apertium -n -d . kaz-morph | cg-proc kaz.rlx.bin -s 3"))) ;; only the first 3 sections so that mapping rules are excluded
+
+(define (kaz-disam s)
+  (parameterize ([current-directory (pkg-directory "apertium-kaz")])
+    (rash
+     "echo (values s) | apertium -n -d . kaz-disam | cg-conv -lcA")))
--- test.rkt
+++ test.rkt
@@ -10,3 +10,33 @@
 (check-equal?
  (kaz-tagger-deterministic "Еуровидение 2010 ән конкурсы Еуровидениенің 55-ші конкурсы болады.")
  "^Еуровидение/Еуровидение<np><al><nom>$ ^2010/2010<num><ord>$ ^ән/ән<n><nom>$ ^конкурсы/конкурс<n><px3sp><nom>$ ^Еуровидениенің/Еуровидение<np><al><gen>$ ^55-ші/55<num><ord>$ ^конкурсы/конкурс<n><px3sp><nom>$ ^болады/бол<v><iv><aor><p3><sg>$^./.<sent>$")
+
+(check-equal?
+ (kaz-disam "Еуровидение 2010 ән конкурсы Еуровидениенің 55-ші конкурсы болады.")
+  "^Еуровидение/Еуровидение<np><al><nom><#1->4><@nmod:poss>$^2010/2010<num><ord><#2->4><@amod>$^ән/ән<n><nom><#3->4><@nmod:poss>$^конкурсы/конкурс<n><px3sp><nom><#4->7><@nsubj>$^Еуровидениенің/Еуровидение<np><al><gen><#5->7><@nmod:poss>$^55-ші/55<num><ord><#6->7><@amod>$^конкурсы/конкурс<n><px3sp><nom><#7->0><@root>$^болады/бол<v><iv><aor><p3><sg><#8->7><@cop>$^./.<sent><#9->7><@punct>$")
+
+
+(check-equal?
+ (kaz-morph "Қой, қазақ бұлармен не ғып соғыса алсын?")
+ "^Қой/ғой<mod_ass>/қой<n><nom>/қой<n><attr>/қой<vaux><imp><p2><sg>/қой<v><tv><imp><p2><sg>/қой<n><nom>+е<cop><aor><p3><pl>/қой<n><nom>+е<cop><aor><p3><sg>$^,/,<cm>$ ^қазақ/қазақ<n><nom>/қазақ<n><attr>/қазақ<n><nom>+е<cop><aor><p3><pl>/қазақ<n><nom>+е<cop><aor><p3><sg>$ ^бұлармен/бұл<prn><dem><pl><ins>$ ^не/не<cnjcoo>/не<prn><itg><nom>/не<prn><itg><nom>+е<cop><aor><p3><pl>/не<prn><itg><nom>+е<cop><aor><p3><sg>$ ^ғып/қыл<v><tv><gna_perf>/қыл<v><tv><prc_perf>$ ^соғыса/соқ<v><tv><coop><gna_impf>/соқ<v><tv><coop><prc_impf>/соқ<v><iv><coop><gna_impf>/соқ<v><iv><coop><prc_impf>/соғыс<v><iv><gna_impf>/соғыс<v><iv><prc_impf>$ ^алсын/ал<vaux><opt><p3><sg>/ал<vaux><opt><p3><pl>/ал<v><tv><opt><p3><sg>/ал<v><tv><opt><p3><pl>$^?/?<sent>$")
+
+(check-equal?
+ (kaz-tagger-deterministic "Қой, қазақ бұлармен не ғып соғыса алсын?")
+ "^Қой/қой<v><tv><imp><p2><sg>$^,/,<cm>$ ^қазақ/қазақ<n><nom>$ ^бұлармен/бұл<prn><dem><pl><ins>$ ^не/не<prn><itg><nom>$ ^ғып/қыл<v><tv><gna_perf>$ ^соғыса/соғыс<v><iv><prc_impf>$ ^алсын/ал<vaux><opt><p3><sg>$^?/?<sent>$")
+
+(check-equal?
+ (kaz-disam "Қой, қазақ бұлармен не ғып соғыса алсын?")
+ "^Қой/қой<v><tv><imp><p2><sg><#1->0><@root>$^,/,<cm><#2->1><@punct>$^қазақ/қазақ<n><nom><#3->7><@nsubj>$^бұлармен/бұл<prn><dem><pl><ins><#4->7><@obl>$^не/не<prn><itg><nom><#5->6><@obj>$^ғып/қыл<v><tv><gna_perf><#6->7><@advcl>$^соғыса/соғыс<v><iv><prc_impf><#7->1><@parataxis>$^алсын/ал<vaux><opt><p3><sg><#8->7><@aux>$^?/?<sent><#9->1><@punct>$")
+
+
+(check-equal?
+ (kaz-morph "Қазақстан осы өңірдегі бейбітшілікті қолдайды.")
+ "^Қазақстан/Қазақстан<np><top><nom>/Қазақстан<np><top><attr>/Қазақстан<np><top><nom>+е<cop><aor><p3><pl>/Қазақстан<np><top><nom>+е<cop><aor><p3><sg>$ ^осы/осы<det><dem>/осы<prn><dem><nom>$ ^өңірдегі/өңір<n><loc><attr>/өңір<n><loc><subst><nom>/өңір<n><loc><subst><nom>+е<cop><aor><p3><pl>/өңір<n><loc><subst><nom>+е<cop><aor><p3><sg>$ ^бейбітшілікті/бейбітшілік<n><acc>/бейбітшілік<n>+лы<post>$ ^қолдайды/қол<n><sim><acc>/қол<adj><subst><sim><acc>/қолда<v><tv><aor><p3><pl>/қолда<v><tv><aor><p3><sg>$^./.<sent>$")
+
+(check-equal?
+ (kaz-tagger-deterministic "Қазақстан осы өңірдегі бейбітшілікті қолдайды.")
+ "^Қазақстан/Қазақстан<np><top><nom>$ ^осы/осы<det><dem>$ ^өңірдегі/өңір<n><loc><attr>$ ^бейбітшілікті/бейбітшілік<n><acc>$ ^қолдайды/қолда<v><tv><aor><p3><sg>$^./.<sent>$")
+
+(check-equal?
+ (kaz-disam "Қазақстан осы өңірдегі бейбітшілікті қолдайды.")
+ "^Қазақстан/Қазақстан<np><top><nom><#1->5><@nsubj>$^осы/осы<det><dem><#2->3><@det>$^өңірдегі/өңір<n><loc><attr><#3->4><@amod>$^бейбітшілікті/бейбітшілік<n><acc><#4->5><@obj>$^қолдайды/қолда<v><tv><aor><p3><sg><#5->0><@root>$^./.<sent><#6->5><@punct>$")
